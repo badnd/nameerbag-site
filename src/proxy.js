3e7e7937bce975fbf63fcf0ded16d326b216a292
badnd <232119507@qq.com>
2026-07-10 18:02:58 +0800

    Add URL canonicalization redirects

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,30 @@
 import { NextResponse } from 'next/server';
 
+const CANONICAL_HOST = 'www.nameerbag.com';
+const APEX_HOST = 'nameerbag.com';
+
+function canonicalPath(pathname) {
+  if (pathname.startsWith('/_next') || pathname.startsWith('/assets') || /\.[^/]+$/.test(pathname)) {
+    return pathname;
+  }
+  return pathname.toLowerCase();
+}
+
 export function proxy(request) {
+  const host = request.headers.get('host')?.toLowerCase().split(':')[0];
+  const pathname = request.nextUrl.pathname;
+  const targetPath = canonicalPath(pathname);
+  const needsWww = host === APEX_HOST;
+  const needsLowercase = targetPath !== pathname;
+
+  if ((host === APEX_HOST || host === CANONICAL_HOST) && (needsWww || needsLowercase)) {
+    const url = request.nextUrl.clone();
+    url.protocol = 'https:';
+    url.host = CANONICAL_HOST;
+    url.pathname = targetPath;
+    return NextResponse.redirect(url, 301);
+  }
+
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-pathname', request.nextUrl.pathname);
   return NextResponse.next({ request: { headers: requestHeaders } });
